caf8a0148b2eb1771ac340e62a0c7d03ea7f71b0
lylJson <34184776+lylJson@users.noreply.github.com>
2017-12-25 12:40:51 +0800

    add main

---
+++ src/main/java/com/lyl/test/TestGit.java
@@ -0,0 +1,12 @@
+package com.lyl.test;
+
+/**
+ * class_name: TestGit
+ * package: com.lyl.test
+ * describe: TODO
+ * create_user: lyl
+ * create_date: 12/25/2017
+ * create_time: 12:36 PM
+ **/
+public class TestGit {
+}
